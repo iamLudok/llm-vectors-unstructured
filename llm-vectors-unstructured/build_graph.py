273ca30973e4c975dfc64118d3d82212366bfa53
Luken Iriondo <122362676+iamLudok@users.noreply.github.com>
2025-10-06 13:38:34 +0000

    Eliminar archivo de ejemplo .env y actualizar scripts para integrar funciones de embeddings y consultas en Neo4j

--- llm-vectors-unstructured/build_graph.py
+++ llm-vectors-unstructured/build_graph.py
@@ -6,6 +6,7 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from openai import OpenAI
 from neo4j import GraphDatabase
+from textblob import TextBlob
 
 COURSES_PATH = "llm-vectors-unstructured/data/asciidoc"
 
@@ -20,16 +21,60 @@ text_splitter = CharacterTextSplitter(
 
 chunks = text_splitter.split_documents(docs)
 
-# Create a function to get the embedding
+def get_embedding(llm, text):
+    response = llm.embeddings.create(
+            input=text,
+            model="text-embedding-ada-002"
+        )
+    return response.data[0].embedding
 
-# Create a function to get the course data
+def get_course_data(llm, chunk):
+    data = {}
 
-# Create OpenAI object
+    path = chunk.metadata['source'].split(os.path.sep)
 
-# Connect to Neo4j
+    data['course'] = path[-6]
+    data['module'] = path[-4]
+    data['lesson'] = path[-2]
+    data['url'] = f"https://graphacademy.neo4j.com/courses/{data['course']}/{data['module']}/{data['lesson']}"
+    data['text'] = chunk.page_content
+    data['embedding'] = get_embedding(llm, data['text'])
+    data['topics'] = TextBlob(data['text']).noun_phrases
 
-# Create a function to run the Cypher query
+    return data
 
-# Iterate through the chunks and create the graph
+def create_chunk(tx, data):
+    tx.run("""
+        MERGE (c:Course {name: $course})
+        MERGE (c)-[:HAS_MODULE]->(m:Module{name: $module})
+        MERGE (m)-[:HAS_LESSON]->(l:Lesson{name: $lesson, url: $url})
+        MERGE (l)-[:CONTAINS]->(p:Paragraph{text: $text})
+        WITH p
+        CALL db.create.setNodeVectorProperty(p, "embedding", $embedding)
+           
+        FOREACH (topic in $topics |
+            MERGE (t:Topic {name: topic})
+            MERGE (p)-[:MENTIONS]->(t)
+        )
+        """, 
+        data
+        )
 
-# Close the neo4j driver
+llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+driver = GraphDatabase.driver(
+    os.getenv('NEO4J_URI'),
+    auth=(
+        os.getenv('NEO4J_USERNAME'),
+        os.getenv('NEO4J_PASSWORD')
+    )
+)
+driver.verify_connectivity()
+for chunk in chunks:
+    with driver.session(database="neo4j") as session:
+        
+        session.execute_write(
+            create_chunk,
+            get_course_data(llm, chunk)
+        )
+driver.close()
